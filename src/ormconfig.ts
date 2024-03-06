967a7618bc6fda50da1faacda3648c2627d11691
mubarak23 <mubarakaminu340@gmail.com>
2024-03-06 03:57:18 +0100

    fix db setup

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -6,9 +6,9 @@ const config: ConnectionOptions = {
   type: "postgres",
   host: process.env.DATABASE_HOSTNAME,
   port: Number(process.env.DATABASE_PORT),
-  username: 'postgres', // process.env.DATABASE_USERNAME,
-  password: 'pass123', // process.env.DATABASE_PASSWORD,
-  database: 'support_service_dev', // process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database:  process.env.DATABASE_NAME,
   entities: [`${__dirname  }/entity/**/*{.ts,.js}`],
   migrations: [
    
